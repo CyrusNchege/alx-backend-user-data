0dd7009956cfcbf0a2f8ab64c80fd0f9c0513f0b
CyrusNchege <cyruschegecc@gmail.com>
2023-11-07 16:19:41 +0300

    Refactor auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -16,10 +16,6 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
-if getenv("AUTH_TYPE") == "auth":
-    auth = Auth()
-elif getenv("AUTH_TYPE") == "basic_auth":
-    auth = BasicAuth()
 
 @app.errorhandler(404)
 def not_found(error) -> str:
