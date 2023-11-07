0f1fc94251d7927220635c10cd2cd0240321c94a
CyrusNchege <cyruschegecc@gmail.com>
2023-11-07 16:16:01 +0300

    Refactor auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -4,6 +4,8 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -12,7 +14,12 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
 
+if getenv("AUTH_TYPE") == "auth":
+    auth = Auth()
+elif getenv("AUTH_TYPE") == "basic_auth":
+    auth = BasicAuth()
 
 @app.errorhandler(404)
 def not_found(error) -> str:
